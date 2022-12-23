ece67df568364750f5259ac25e8ea070d44342ba
yusuf <yusufsoyler5222@gmail.com>
2022-12-23 23:22:32 +0300

    Duck Hunt

---
+++ ÖrdekOyunu/ÖrdekOyunu/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace ÖrdekOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.lblisabetlitik = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbltoplamtik = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ordek = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ordek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(53, 650);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(88, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 126);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(160, 271);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "ZOR";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(160, 199);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ORTA";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(160, 126);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "KOLAY";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.lblpuan);
+            this.panel2.Controls.Add(this.lblisabetlitik);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.lbltoplamtik);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(1059, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(71, 650);
+            this.panel2.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(166, 248);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 16);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "PUAN:";
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Location = new System.Drawing.Point(283, 248);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(44, 16);
+            this.lblpuan.TabIndex = 5;
+            this.lblpuan.Text = "label7";
+            // 
+            // lblisabetlitik
+            // 
+            this.lblisabetlitik.AutoSize = true;
+            this.lblisabetlitik.Location = new System.Drawing.Point(283, 174);
+            this.lblisabetlitik.Name = "lblisabetlitik";
+            this.lblisabetlitik.Size = new System.Drawing.Size(44, 16);
+            this.lblisabetlitik.TabIndex = 4;
+            this.lblisabetlitik.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "İSABETLİ TİK:";
+            // 
+            // lbltoplamtik
+            // 
+            this.lbltoplamtik.AutoSize = true;
+            this.lbltoplamtik.Location = new System.Drawing.Point(283, 103);
+            this.lbltoplamtik.Name = "lbltoplamtik";
+            this.lbltoplamtik.Size = new System.Drawing.Size(44, 16);
+            this.lbltoplamtik.TabIndex = 2;
+            this.lbltoplamtik.Text = "label4";
+            this.lbltoplamtik.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "TOPLAM TİK:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(121, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 124);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "YENİDEN OYNA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(533, 614);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SÜRE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(611, 614);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "20";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ordek
+            // 
+            this.ordek.BackColor = System.Drawing.Color.Transparent;
+            this.ordek.Image = ((System.Drawing.Image)(resources.GetObject("ordek.Image")));
+            this.ordek.Location = new System.Drawing.Point(497, 272);
+            this.ordek.Name = "ordek";
+            this.ordek.Size = new System.Drawing.Size(82, 85);
+            this.ordek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ordek.TabIndex = 4;
+            this.ordek.TabStop = false;
+            this.ordek.Click += new System.EventHandler(this.ordek_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1132, 654);
+            this.Controls.Add(this.ordek);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "zz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ordek)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.Label lblisabetlitik;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbltoplamtik;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox ordek;
+    }
+}
+
---
+++ ÖrdekOyunu/ÖrdekOyunu/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ÖrdekOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public
+            Random rnd = new Random();
+
+        int sure = 20;
+        int toplamtik = 0;
+        int isabetlitik = 0;
+            int puan = 0;
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Dock = DockStyle.Fill;
+            panel2.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            timer2.Start();
+
+            if (radioButton1.Checked = true) { timer2.Interval = 800; }
+            if (radioButton2.Checked = true) { timer2.Interval = 600; }
+            if (radioButton3.Checked = true) { timer2.Interval = 450; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = Convert.ToString("SÜRE:" + sure);
+            if (sure == 0)
+            {
+                timer1.Stop(); timer2.Stop();
+
+                panel2.Dock = DockStyle.Fill;
+                panel2.Show();
+
+                toplamtik += isabetlitik;
+
+                lbltoplamtik.Text = Convert.ToString("TOPLAM TIKLAMA:" + toplamtik);
+                lblisabetlitik.Text = Convert.ToString("İSABETLİ TIKLAMA:" + isabetlitik);
+                lblpuan.Text = Convert.ToString("PUAN:" + puan);
+
+
+            }
+            sure--;
+
+
+        }
+
+        
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            ordek.Location = new Point(Convert.ToInt32(rnd.Next(10, 700)));
+
+
+                Convert.ToInt32(rnd.Next(10,250));  
+
+        }
+
+        private void ordek_Click(object sender, EventArgs e)
+        {
+            isabetlitik++;
+            puan += 5;
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            toplamtik++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sure = 20;
+            toplamtik = 0;
+            isabetlitik = 0;
+            puan = 0;
+
+            radioButton1.Checked = true;
+            panel2.Hide();
+            panel1.Show();
+            ordek.Location = new Point(350, 110);
+
+        }
+    }
+    }
+
