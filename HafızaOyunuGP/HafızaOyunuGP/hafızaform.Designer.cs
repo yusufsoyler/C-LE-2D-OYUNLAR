0d59ad106e2ad16e1a25a0a881c19781f8468fc2
yusuf <yusufsoyler5222@gmail.com>
2022-12-23 23:21:33 +0300

    Hafıza Oyunu

---
+++ HafızaOyunuGP/HafızaOyunuGP/hafızaform.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace HafızaOyunuGP
+{
+    partial class hafızaform
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(210, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 101);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button2.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Location = new System.Drawing.Point(350, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 101);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button3.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(492, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 101);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button4.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button4.Location = new System.Drawing.Point(634, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 101);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button5.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button5.Location = new System.Drawing.Point(210, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 101);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button6.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Location = new System.Drawing.Point(350, 213);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 101);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button7.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button7.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button7.Location = new System.Drawing.Point(492, 213);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 101);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button8.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Location = new System.Drawing.Point(634, 213);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(142, 101);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button9.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button9.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button9.Location = new System.Drawing.Point(210, 314);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(142, 101);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button10.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button10.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button10.Location = new System.Drawing.Point(350, 314);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(142, 101);
+            this.button10.TabIndex = 0;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button11.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button11.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button11.Location = new System.Drawing.Point(492, 314);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(142, 101);
+            this.button11.TabIndex = 0;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button12.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button12.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button12.Location = new System.Drawing.Point(634, 314);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(142, 101);
+            this.button12.TabIndex = 0;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button13.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button13.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button13.Location = new System.Drawing.Point(210, 415);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(142, 101);
+            this.button13.TabIndex = 0;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button14.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button14.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button14.Location = new System.Drawing.Point(350, 415);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(142, 101);
+            this.button14.TabIndex = 0;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button15.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button15.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button15.Location = new System.Drawing.Point(492, 415);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(142, 101);
+            this.button15.TabIndex = 0;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button16.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button16.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button16.Location = new System.Drawing.Point(634, 415);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(142, 101);
+            this.button16.TabIndex = 0;
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.Buton_Click);
+            // 
+            // hafızaform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1016, 653);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Name = "hafızaform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
+
---
+++ HafızaOyunuGP/HafızaOyunuGP/hafızaform.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HafızaOyunuGP
+{
+    public partial class hafızaform : Form
+    {
+        List<string> icons = new List<string>()
+        {
+            "!",",","b,","k","v","w","z","N", // ikonlarımızı burada belirledik.Buttonların üzerinde gözükecek olan ikonlar.
+            "!",",","b,","k","v","w","z","N"
+        };
+        Random rnd = new Random(); // random sınıfını kullanarak nesne ürettik.
+        int randomindex;
+        Timer t=new Timer(); // kartların arka yüzünü dönmeden önce göreceğimiz süreyi belirleyecek timerı tanımladık.
+        Timer t2 = new Timer(); // seçilen buttonların ters yüzüne tekrar döneceği süreyi tanımladık.
+        Button bir, iki;
+
+        public hafızaform()
+        {
+            InitializeComponent();
+            t.Tick += T_Tick; // timerin tick komutu kullanılarak süreler belirlendi.
+            t.Start();
+            t.Interval = 3000;
+            show();
+            t2.Tick += T2_Tick;
+        }
+
+        private void T2_Tick(object sender, EventArgs e)
+        {
+            t2.Stop();
+            bir.ForeColor = bir.BackColor;
+            iki.ForeColor = iki.BackColor;
+            bir = null;
+            iki = null;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            t.Stop();
+            foreach (Button item in Controls)
+            {
+                item.ForeColor = item.BackColor;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void show()
+        {
+            Button btn;
+            foreach (Button item in Controls)
+            {
+                btn = item as Button;
+                randomindex = rnd.Next(icons.Count); // ikonların sayısı kadar rastgele buttonların hepsine şekiller dagıtıldı.
+                btn.Text = icons[randomindex]; // buttonların textine de rastgele dagılmış ikonlar yerleştirildi.
+                btn.ForeColor = Color.Black;
+                icons.RemoveAt(randomindex); // aynı ikonlar fazla kez tekrar etmemesi için removeat komutu kullanıldı.
+
+            }
+        }
+        int sayac = 0;
+        private void Buton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (bir == null)
+            {
+               bir = btn;
+               bir.ForeColor= Color.Black;
+                return;
+            }
+            iki = btn;
+            iki.ForeColor=Color.Black;
+            if(bir.Text == iki.Text) // seçilen kartlar aynı gelirse arka plan renkleri ayarlandı ve tekrar ters dönmemeleri sağlandı.
+            {
+                bir.ForeColor= Color.Black;
+                iki.ForeColor= Color.Black;
+                bir = null; // buttonlar bu işlemden sonra tekrar kullanılması için null atandı.
+                iki = null;
+                sayac++;
+                if (sayac == 8) // toplam 8 çift bulunduktan sonra oyunun sonlanması için close komutu kullanıldı.
+                {
+                    MessageBox.Show("Oyun Bitti.");
+                    Close();
+                }
+            }
+            else
+            {
+                t2.Start(); // eğer ki seçilen iki kart aynı değilse 1 saniye aradan sonra ters dönmeleri için gerekli süre tanımlandı.
+                t2.Interval = 1000;
+            }
+        }
+    }
+}
